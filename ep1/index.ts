b48e8b1df9d0634e538e832234118c5f77d904ed
danisakunsada <danis.nr12345@gmail.com>
2024-07-19 16:12:54 +0700

    งาน2

--- ep1/index.ts
+++ ep1/index.ts
@@ -6,3 +6,12 @@ let อยากกลับบ้าน:boolean = false
 console.log(firstname)
 console.log(y)
 console.log(อยากกลับบ้าน)
+
+
+
+const a:number = 1
+const b:number = 2
+
+console.log (a == b)
+console.log (a > b)
+console.log(a<= b)
