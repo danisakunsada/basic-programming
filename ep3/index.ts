ebeec2fd845092bae90b63344bc391a62fa7971e
danisakunsada <danis.nr12345@gmail.com>
2024-08-01 13:57:40 +0700

    Update index.ts

--- ep3/index.ts
+++ ep3/index.ts
@@ -53,3 +53,26 @@ function ps(age:number , salary:number , deposit:number){
     }
 }
 ps(16,70000,500000)
+
+
+function ค่าBMI(weight_kg: number, height_m: number) {
+    return   weight_kg / height_m **2
+
+    }
+
+function criterion(weight_kg: number, height_m: number) {
+    let BMI : number = weight _kg / height_m **2
+
+    if (BMI < 18.5 ) {
+        return 'ผอมเกิน'
+    } else if (BMI > 18.5 && BMI < 24) {
+        return 'น้ำหนักปกติ เหมาะสม'
+    } else if (NMI > 24 && BMI < 29.5) {
+        return'อ้วน'
+    } else if (BMI > 29.5 && BMI < 30.0){
+        return'อ้วนมาก'
+    }
+}
+
+console. Log ( ค่าBMI (60,1.65))
+console. log ( criterion (60,1.65))
